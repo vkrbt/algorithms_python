3f8d329840d6c8e75b970ba96fbfcb20c9de4baa
Vladislav Korbut <v.korbut8@gmail.com>
2017-11-14 01:21:50 +0300

    Refactoting

--- dijkstra/search.py
+++ dijkstra/search.py
@@ -1,9 +1,18 @@
 from collections import deque
+from math import inf
+
 
 def search(graph):
-    lengths = {}
-    lengths[0] = 0
-    queue = deque()
+    verticesNumber = len(graph[0])
+    lens = [inf] * verticesNumber
+    used = [False] * verticesNumber
 
-    while queue:
-        current = queue.pop()
+    vertices = range(verticesNumber)
+    for i in vertices:
+        v = None
+        for j in vertices:
+            if not used[j] and (v in None or lens[j] < lens[v]):
+                v = j
+        if lens[v] == inf:
+            break
+        used[v] = True
--- euler/search.py
+++ euler/search.py
@@ -1,17 +1,21 @@
 def find_cycle(graph):
-    stack = []
-    stack.append(0)
-    res = []
-    while stack:
-        current = stack[-1]
-        if sum(graph[current]) == 0:
-            res.append(stack.pop())
-        else:
-            vertex = graph[current].index(1)
-            graph[vertex][current] = 0
-            graph[current][vertex] = 0
-            stack.append(vertex)
-    return res
+    if is_euler(graph):
+        stack = []
+        stack.append(0)
+        res = []
+        while stack:
+            current = stack[-1]
+            if sum(graph[current]) == 0:
+                res.append(stack.pop())
+            else:
+                vertex = graph[current].index(1)
+                graph[vertex][current] = 0
+                graph[current][vertex] = 0
+                stack.append(vertex)
+        return res
+    else:
+        return None
+
 
 def is_euler(graph):
     for row in graph:
