ebaf73b49f9cd5ff0541d0aa84b86b2c60f6ccc7
Vladislav Korbut <v.korbut8@gmail.com>
2017-12-26 09:53:16 +0300

    Refactor smp

--- marriage/marriage.py
+++ marriage/marriage.py
@@ -8,35 +8,33 @@ def marriage(men, women):
     pairs_number = len(men);
     unapaired_man_index = get_unpaired_man(married)
     while unapaired_man_index is not None:
-        pref_woman_index = men[unapaired_man_index].pop(0)        
+        pref_woman_index = men[unapaired_man_index].pop(0)
+
         while unapaired_man_index not in women[pref_woman_index]:
             pref_woman_index = men[unapaired_man_index].pop(0)
+
         man_index = women[pref_woman_index].index(unapaired_man_index)
         married = delete_prev_suggestion(married, pref_woman_index)
-        if man_index == 0:
+
+        if man_index is 0:
             married[unapaired_man_index][pref_woman_index] = accept
             pairs.append((unapaired_man_index, pref_woman_index))
             men = remove_woman_from_men(men, pref_woman_index)
         else:
             married[unapaired_man_index][pref_woman_index] = suggest
-       
+
         women[pref_woman_index] = women[pref_woman_index][:man_index]
         unapaired_man_index = get_unpaired_man(married)
-        if unapaired_man_index is None:
-            unapaired_man_index = get_first_unmarried_man(married);
     return pairs
 
 def get_unpaired_man(married):
-    for man in married:
-        if suggest not in man and accept not in man:
-            return married.index(man)
-    return None
-
-def get_first_unmarried_man(married):
-    for man in married:
-        if accept not in man:
-            return married.index(man)
-    return None
+    unamarried = None
+    for man_index in range(len(married)):
+        if suggest not in married[man_index] and accept not in married[man_index]:
+            return man_index
+        if accept not in married[man_index] and unamarried is None:
+            unamarried = man_index
+    return unamarried
 
 def remove_woman_from_men(men, woman):
     for man in men:
