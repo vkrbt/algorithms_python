f21979e3f1d3fd39a7614bf064a60802d4ee01d9
Vladislav Korbut <v.korbut8@gmail.com>
2017-12-26 11:24:10 +0300

    Fix turing machine

--- turingmachine/main.py
+++ turingmachine/main.py
@@ -1,42 +1,51 @@
-def prepare(num):
-  num = 'a' + num + 'b' * (len(num) + 2)
-  num = list(num)
-  index = 0;
-  while num[index] is not 'b':
-    index += 1
-  index -= 1
-  state = num[index]
-  num[index] = '_'
-  while num[index] is not 'b':
-    index += 1
-  num[index] = state
-  return num
-
+states = dict(
+  line_end = '}',
+  line_start = '{',
+  nums = ['0', '1'],
+  empty = '',
+)
 
 def invert_number(num):
-  num = prepare(num)
+  num = list(states['line_start'] + num + states['line_end'] * (len(num) + 2))
+
   index = 0
-  while num[index] is not '_':
+
+  while num[index] is not states['line_end']:
     index += 1
   index -= 1
   state = num[index]
-  while num[index] is not 'a':
-    num[index] = '_'
-    while num[index] is not 'b':
+  num[index] = states['empty']
+  while num[index] is not states['line_end']:
+    index += 1
+  num[index] = state
+
+  while num[index] is states['empty']:
+    index -= 1
+  state = num[index]
+
+  while state is not states['line_start']:
+    num[index] = states['empty']
+    while num[index] is not states['line_end']:
       index += 1
     num[index] = state
-    while num[index] is '1' or num[index] is '0':
+    while num[index] in states['nums']:
       index -= 1
-    while num[index] is '_':
+    while num[index] is states['empty']:
       index -= 1
     state = num[index]
   index += 1
-  while num[index] is '_':
-    num[index] = 'a'
+  while num[index] is states['empty']:
+    num[index] = states['line_start']
     index += 1
-  return ''.join(num)
+  while states['line_start'] in num:
+    num.remove(states['line_start'])
+  return ''.join(num)[:-1]
 
 
 if __name__ == '__main__':
-  num = '1001011101'
-  print(invert_number(num));
+  num = '1001011100101101'
+  num_reversed = ''.join(reversed(list(num)))
+  num_reversed_turing = invert_number(num)
+  print(num_reversed)
+  print(num_reversed_turing)
+  print('Right' if num_reversed == num_reversed_turing else 'Wrong')
