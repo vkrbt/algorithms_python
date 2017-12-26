c4d1e18e80e562b59f89d874962f4f265590acfe
Vladislav Korbut <v.korbut8@gmail.com>
2017-12-26 10:46:31 +0300

    Fix turing machine

--- turingmachine/main.py
+++ turingmachine/main.py
@@ -1,24 +1,42 @@
 def prepare(num):
-  num = list(num);
-  last_pos = num.index('b') - 1
-  last_state = num[last_pos]
-  num[last_pos] = '_';
-  num[last_pos + 1] = last_state
+  num = 'a' + num + 'b' * (len(num) + 2)
+  num = list(num)
+  index = 0;
+  while num[index] is not 'b':
+    index += 1
+  index -= 1
+  state = num[index]
+  num[index] = '_'
+  while num[index] is not 'b':
+    index += 1
+  num[index] = state
   return num
 
 
 def invert_number(num):
   num = prepare(num)
-  while num[num.index('_') - 1] is not 'a':
-    last_pos = num.index('_') - 1
-    last_state = num[last_pos]
-    num[num.index('b')] = last_state
-    num[last_pos + 1] = 'a'
-    num[last_pos] = '_'
-  num[last_pos] = 'a'
+  index = 0
+  while num[index] is not '_':
+    index += 1
+  index -= 1
+  state = num[index]
+  while num[index] is not 'a':
+    num[index] = '_'
+    while num[index] is not 'b':
+      index += 1
+    num[index] = state
+    while num[index] is '1' or num[index] is '0':
+      index -= 1
+    while num[index] is '_':
+      index -= 1
+    state = num[index]
+  index += 1
+  while num[index] is '_':
+    num[index] = 'a'
+    index += 1
   return ''.join(num)
 
 
 if __name__ == '__main__':
-  num = 'a1001011101bbbbbbbbbbbbbbbbbbbbbb'
+  num = '1001011101'
   print(invert_number(num));
