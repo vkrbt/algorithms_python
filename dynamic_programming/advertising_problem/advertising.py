7c2a8f673182a54a6b3b2087390f9f3cfb1d9100
Vladislav Korbut <v.korbut8@gmail.com>
2018-03-03 18:04:04 +0300

    refactoring

--- dynamic_programming/advertising_problem/advertising.py
+++ dynamic_programming/advertising_problem/advertising.py
@@ -1,12 +1,26 @@
-def find_solution(positions, costs):
+def find_solution(positions, costs, min_len):
   solution = [0, costs[0]]
-  for i in range(2, len(positions)):
-    solution.append(max(solution[i - 1], costs[i] + solution[get_nearest_cost(positions, i)]))
-  return solution
+  solution_positions = [[], [positions[0]]]
+  for i in range(1, len(positions)):
+    max_nearest = get_nearest_pos(positions, i, min_len)
+    new_cost = costs[i] + solution[max_nearest + 1]
+    
+    if new_cost > solution[i]:
+      solution.append(new_cost)
+      prev_positions = solution_positions[max_nearest + 1]
+      prev_positions.append(positions[i])
+      solution_positions.append(prev_positions)
+    else:
+      solution.append(solution[i])
+      solution_positions.append(solution_positions[-1])
+    print(solution, solution_positions)
+  return { 'cost': solution[-1], 'positions': solution_positions[-1]}
 
-def get_nearest_cost(positions, pos):
+def get_nearest_pos(positions, pos, min_len):
   current = positions[pos]
-  pos = pos - 1  
-  while current - positions[pos] < 5:
+  while  pos > 0:
+    if current - positions[pos] > min_len:
+      print(pos)
+      return pos
     pos = pos - 1
-  return pos
+  return -1
--- dynamic_programming/advertising_problem/main.py
+++ dynamic_programming/advertising_problem/main.py
@@ -3,4 +3,4 @@ from advertising import find_solution
 
 if __name__ == '__main__':
   data = json.load(open('./dynamic_programming/advertising_problem/data.json'))
-  print(find_solution(data['positions'], data['costs']))
+  print(find_solution(data['positions'], data['costs'], 5))
