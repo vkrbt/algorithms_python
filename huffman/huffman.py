8464342de9c6910b79d3db74e0c97b8f4596b721
Vladislav Korbut <v.korbut8@gmail.com>
2018-05-27 18:30:55 +0300

    Add huffman algo

--- huffman/huffman.py
+++ huffman/huffman.py
@@ -1,20 +1,23 @@
-from collections import Counter  # словарь в котором для каждого объекта поддерживается счетчик
+from collections import Counter
 from collections import namedtuple
 import heapq
 
 
-class Node(namedtuple("Node", ["left", "right"])):
+class Node(namedtuple('Node', ['left', 'right'])):
     def walk(self, code, acc):
-        self.left.walk(code, acc + "0")
-        self.right.walk(code, acc + "1")
+        self.left.walk(code, acc + '0')
+        self.right.walk(code, acc + '1')
 
-class Leaf(namedtuple("Leaf", ["char"])):
+
+class Leaf(namedtuple('Leaf', ['char'])):
     def walk(self, code, acc):
-        code[self.char] = acc or "0"
+        code[self.char] = acc or '0'
+
 
 def get_frequency():
     frequency = {}
-    def add_letter(letter = None):
+
+    def add_letter(letter=None):
         if letter is None:
             return frequency
         if letter in frequency:
@@ -23,41 +26,69 @@ def get_frequency():
             frequency[letter] = 1
     return add_letter
 
-def get_map(letter_map = {}):
-    h = []
-    for letter, frequency in letter_map.items():
-        h.append((frequency, len(h), Leaf(letter)))
-    heapq.heapify(h)
-    count = len(h)
 
-    while len(h) > 1:
-        freq1, _, left = heapq.heappop(h)
-        freq2, _, right = heapq.heappop(h)
-        heapq.heappush(h, (freq1 + freq2, count, Node(left, right)))
+def get_map(letter_map={}):
+    queue = []
+    for letter, frequency in letter_map.items():
+        queue.append((frequency, len(queue), Leaf(letter)))
+    heapq.heapify(queue)
+    count = len(queue)
+
+    while len(queue) > 1:
+        freq1, _, left = heapq.heappop(queue)
+        freq2, _, right = heapq.heappop(queue)
+        heapq.heappush(queue, (freq1 + freq2, count, Node(left, right)))
 
         count += 1
     codes_map = {}
-    if h:
-        [(_freq, _count, root)] = h
-        root.walk(codes_map, "")
+    if queue:
+        [(_freq, _, root)] = queue
+        root.walk(codes_map, '')
     return codes_map
 
+
 def encode(codes_map):
-    encoded = ''
-    def add_letter(letter = None):
+    last_bits = ''
+    file = open('huffman/encoded', 'wb')
+
+    def add_letter(letter=None):
+        nonlocal last_bits
         if letter is None:
-            return encoded    
-        encoded += codes_map[letter]
+            need_zeros = 8 - len(last_bits)
+            last_bits = last_bits.ljust(8, '0')
+            byte = bytes([int(last_bits, 2)])
+            file.write(byte)
+            byte = bytes([need_zeros])
+            print(need_zeros)
+            file.write(byte)
+            file.close()
+            return
+        last_bits += codes_map[letter]
+        if len(last_bits) > 8:
+            byte = bytes([int(last_bits[:8], 2)])
+            last_bits = last_bits[8:]
+            file.write(byte)
     return add_letter
 
-def huffman_decode(encoded, code):  # функция декодирования исходной строки по кодам Хаффмана
-    sx =[]  # инициализируем массив символов раскодированной строки
-    enc_ch = ""  # инициализируем значение закодированного символа
-    for ch in encoded:  # обойдем закодированную строку по символам
-        enc_ch += ch  # добавим текущий символ к строке закодированного символа
-        for dec_ch in code:  # постараемся найти закодированный символ в словаре кодов
-            if code.get(dec_ch) == enc_ch:  # если закодированный символ найден,
-                sx.append(dec_ch)  # добавим значение раскодированного символа к массиву раскодированной строки
-                enc_ch = ""  # обнулим значение закодированного символа
-                break
-    return "".join(sx)
+
+def huffman_decode(codes_map):
+    encoded = []
+    file_in = open('huffman/encoded', 'rb')
+    encoded_ch = ''
+
+    last_pos = list(file_in)[::-1]
+    last_zeros = last_pos[0][len(last_pos[0]) - 1]
+    file_in.seek(0, 0)
+    for line in file_in:
+        for byte in line:
+            encoded_ch += str(bin(byte))[2:].rjust(8, '0')
+    encoded_ch = encoded_ch[:len(encoded_ch) - 8 - last_zeros]
+    while encoded_ch:
+        for i in range(1, len(encoded_ch) + 1):
+            for letter, code in codes_map.items():
+                if code == encoded_ch[:i]:
+                    encoded.append(letter)
+                    encoded_ch = encoded_ch[i:]
+
+    file_out = open('huffman/decoded', 'w')
+    file_out.write(''.join(encoded))
--- huffman/main.py
+++ huffman/main.py
@@ -1,18 +1,20 @@
-from huffman import get_frequency, get_map, encode
+from huffman import get_frequency, get_map, encode, huffman_decode
 
 if __name__ == '__main__':
-    file  = open('huffman/text.txt', 'r')
+    file = open('huffman/text.txt', 'r')
     add_letter = get_frequency()
     for line in file:
         for ch in line:
             add_letter(ch)
     file.close()
-    
-    codes_map = add_letter()
 
-    encoded = encode(codes_map)
-                                             # в соответствующий код и конкатенируем результат
-    print(len(codes_map), len(encoded))  # выведем число символов и длину закодированной строки
-    for ch in sorted(codes_map): # обойдем символы в словаре в алфавитном порядке с помощью функции sorted()
-        print("{}: {}".format(ch, codes_map[ch]))  # выведем символ и соответствующий ему код
-    print(encoded)
+    codes_map = get_map(add_letter())
+
+    add_letter = encode(codes_map)
+    file = open('huffman/text.txt', 'r')
+    for line in file:
+        for ch in line:
+            add_letter(ch)
+    add_letter()
+    file.close()
+    huffman_decode(codes_map)
